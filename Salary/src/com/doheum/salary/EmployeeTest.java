e704ed269603e7d69bbcf0cbec0bf81a15cf7c99
a <a>
2019-09-21 13:20:39 +0900

    급여계산(2)

--- Salary/src/com/doheum/salary/EmployeeTest.java
+++ Salary/src/com/doheum/salary/EmployeeTest.java
@@ -2,9 +2,15 @@ package com.doheum.salary;
 
 public class EmployeeTest {
 	public static void main(String[] args) {
+		Temporary te = new Temporary("홍길동", 28, "대구시", "위인전");
+		te.setWorkTime(100);
+		te.printInfo();
+		
+		/*
 		Regular re = new Regular("홍길동", 28, "대구시", "위인전");		
 		re.setSalary(1_000_000);
 		re.printInfo();
+		*/
 		/*
 		Employee em = new Employee("홍길동", 28, "대구시", "위인전");
 		em.printInfo();
--- Salary/src/com/doheum/salary/Temporary.java
+++ Salary/src/com/doheum/salary/Temporary.java
@@ -1,5 +1,30 @@
 package com.doheum.salary;
 
-public class Temporary {
+public class Temporary extends Employee {
+	private int workTime;
+	private int payPerTime;
+	
+	public Temporary(String name, int age, String addr, String department) {
+		super(name, age, addr, department);
+		this.payPerTime = 10000;
+	}
 
+	public void setWorkTime(int workTime) {
+		this.workTime = workTime;
+		this.salary = this.workTime * this.payPerTime;
+	}
+	
+	@Override
+	public void printInfo() {
+		System.out.printf("비정규직  일한시간: %d,  급여: %,d\n", 
+				this.workTime, this.salary);
+	}
+	
+	
 }
+
+
+
+
+
+
